188ddbac6df78241ab51a51f35061db31ae0083b
Duba Abel Bitrus <87368904+Abeltrust@users.noreply.github.com>
2026-06-10 11:11:57 -0700

    Fix image file extension for zoeMoviePoster

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import { PageTransition } from '../components/PageTransition';
 import { Play, Film, Calendar, Compass, Sparkles, Tv, ArrowRight } from 'lucide-react';
 import React, { useState } from 'react';
-import zoeMoviePoster from '../assets/images/ZOE.jpg';
+import zoeMoviePoster from '../assets/images/ZOE.JPG';
 import rainbowChristmasPoster from '../assets/images/rfc.jpg';
 import sunsglory from '../assets/images/sgs.jpg';
 
