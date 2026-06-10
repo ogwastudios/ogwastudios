b63decdffbf21c941757b598a4ca1278fbf81f76
Duba Abel Bitrus <87368904+Abeltrust@users.noreply.github.com>
2026-06-10 10:30:48 -0700

    Fix typo in image import path for ogwaHomeImage

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import { PageTransition } from '../components/PageTransition';
 import { Play, Film, Calendar, Compass, Sparkles, Tv, ArrowRight } from 'lucide-react';
 import React, { useState } from 'react';
-import ogwaHomeImage from '../assets/images/ogwa_home_mage.jpg';
+import ogwaHomeImage from '../assets/images/home_mage.jpg';
 import rainbowChristmasPoster from '../assets/images/rfc.jpg';
 import sunsglory from '../assets/images/sgs.jpg';
 
