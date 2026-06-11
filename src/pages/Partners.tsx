413564b8e2a736c459d9b772ebf781a82db57898
Duba Abel Bitrus <abelskid002@gmail.com>
2026-06-10 23:50:56 -0700

    feat: implement Partners page with alliance details and co-creation information

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -34,7 +34,7 @@ const partnersGains = [
 import theNollyWoodReporter from '../assets/images/thwr-logo.jpeg';
 import bc from '../assets/images/bc-logo.png';
 import cc from '../assets/images/cc-logo.png';
-import cep from '../assets/images/cep-logo.png';
+import cep from '../assets/images/cep-Logo.png';
 const currentPartners = [
   {
     name: "CREATIVE ALLIANCE",
