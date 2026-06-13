d745ac1fb23dbd9b9871cae08459c9ba121e6248
Duba Abel Bitrus <abelskid002@gmail.com>
2026-06-13 07:15:35 -0700

    feat: create projects page with responsive grid and interactive video preview portal

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -118,7 +118,7 @@ export const Projects = () => {
                   <iframe 
                     width="100%" 
                     height="100%" 
-                    src="https://www.youtube.com/watch?v=bkJ-jIy6bBk" 
+                    src="https://www.youtube.com/embed/bkJ-jIy6bBk?si=m_cu-jtOwyWoXReP" 
                     title="Teaser Reel" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
