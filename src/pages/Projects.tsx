36e3ed64743b29b3916d59f7330a5b1875b7ca49
Duba Abel Bitrus <abelskid002@gmail.com>
2026-06-13 07:19:44 -0700

    feat: implement Projects page with interactive cinematic portal and project gallery

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -118,7 +118,7 @@ export const Projects = () => {
                   <iframe 
                     width="100%" 
                     height="100%" 
-                    src="https://www.youtube.com/embed/bkJ-jIy6bBk?si=m_cu-jtOwyWoXReP" 
+                    src="https://www.youtube.com/embed/bkJ-jIy6bBk?autoplay=1&mute=1" 
                     title="Teaser Reel" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
