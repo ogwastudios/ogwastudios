18903b1fe43724fea186d0df7f6748ca7617dabf
ogwastudios <ogwatvmedia@gmail.com>
2026-08-06 04:51:06 +0100

    Replace Checkers Custard logo with My Oga Doctor

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -33,7 +33,7 @@ const partnersGains = [
 
 import theNollyWoodReporter from '../assets/images/thwr-logo.jpeg';
 import bc from '../assets/images/bc-logo.png';
-import cc from '../assets/images/cc-logo.png';
+import cc from "../assets/images/cc-logo.png.jpeg";
 import cep from '../assets/images/cep-logo.png';
 import ulanzi from '../assets/images/ulanzi-logo.png';
 
